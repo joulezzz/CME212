54e4a44401140b98f8b093998f4e7b9432a67c97
Julio Martinez <julio@DN525ehe.SUNet>
2017-02-01 19:29:39 -0800

    update

--- shortest_path.cpp
+++ shortest_path.cpp
@@ -88,7 +88,7 @@ int shortest_path_lengths(GraphType& g, NodeType& root)
       if (node2.value() == -1){
         node2.value() = n.value() + 1;
         if (node2.value() > current_max) current_max = node2.value();
-        q.pugh(node2);
+        q.push(node2);
       }
     }
   }
