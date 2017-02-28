dd15f3dedb0ebe148e1894d808c71de214d87898
Julio Martinez <juliomz@stanford.edu>
2017-02-28 13:48:46 -0800

    added mtl stuff and made simple changes

--- mtl_test.cpp
+++ mtl_test.cpp
@@ -12,6 +12,59 @@
 // Define a IdentityMatrix that interfaces with MTL
 
 
+
+
+
+class IdentityMatrix {
+  public:
+  	IdentityMatrix(std::size_t nrows, std::size_t ncols) : nrows_(nrows), ncols_(ncols) {}
+
+	/** Compute the product of a vector with this identity matrix
+	 */
+	template <typename Vector>
+	Vector operator*(const Vector& x) const {
+		return x;
+	}
+
+	/** The numner of elements in the matrix. */
+	inline std::size_t size(const IdentityMatrix& A){
+		return nrows_*ncols_;
+	}
+	/** The numbner of rows in the matrix. */
+	inline std::size_t num_rows(const IdentityMatrix& A){
+		return nrows_;
+	}
+	/** The number of columns in the matrix. */
+	inline std::size_t num_cols(const IdentityMatrix& A){
+		return ncols_;
+	}
+
+	/** Helper function to perfom multiplication. Allows for delayed 
+	 *  evaluation of results.
+	 *  Assign :: apply(a, b) resolves to an assignment operation such as 
+	 *     a += b, a -= b, or a = b.
+	 *  @pre @a size(v) == size(w) */
+	template <typename VectorIn, typename VectorOut, typename Assign>
+	void mult (const VectorIn& v, VectorOut& w, Assign) const {
+	    assert(size(v) == size(w));
+	    Assign::apply(w,v);
+	}
+
+	/** Matvec forward to MTL's lazy mat_cvec_multiplier oeprator */
+	template <typename Vector> 
+	mtl::vec::mat_cvec_multiplier<IdentityMatrix, Vector>
+	operator*(const Vector& v) const {
+		return {*this, v};
+	}
+
+
+  private:
+  	// Empty
+  	std::size_t nrows_;
+  	std::size_t ncols_;
+
+};
+
 /** Traits that MTL used to detmerine propperties of our IdentityMatrix. */
 namespace mtl {
 namespace ashape {
@@ -32,57 +85,6 @@ struct Collection<IdentityMatrix> {
 };
 } // end namespace
 
-
-
-class IdentityMatrix {
-  public:
-  	IdentityMatrix(std::size_t nrows, std::size_t ncols) : nrows_(nrows), ncols_(ncols) {}
-
-	/** Compute the product of a vector with this identity matrix
-	 */
-	template <typename Vector>
-	Vector operator*(const Vector& x) const {
-		return x;
-	}
-
-	/** The numner of elements in the matrix. */
-	inline std::size_t size(const IdentityMatrix& A){
-		return num_rows_*num_cols_;
-	}
-	/** The numbner of rows in the matrix. */
-	inline std::size_t num_rows(const IdentityMatrix& A){
-		return num_rows_;
-	}
-	/** The number of columns in the matrix. */
-	inline std::size_t num_cols(const IdentityMatrix& A){
-		return num_cols_;
-	}
-
-	/** Helper function to perfom multiplication. Allows for delayed 
-	 *  evaluation of results.
-	 *  Assign :: apply(a, b) resolves to an assignment operation such as 
-	 *     a += b, a -= b, or a = b.
-	 *  @pre @a size(v) == size(w) */
-	template <typename VectorIn, typename VectorOut, typename Assign>
-	void mult (const VectorIn& v, VectorOut& w, Assign) const {
-	    assert(size(v) == size(w));
-	    Assign::apply(w,v);
-	}
-
-	/** Matvec forward to MTL's lazy mat_cvec_multiplier oeprator */
-	template <typename Vector> 
-	mtl::vec::mat_cvec_multiplier<IdentityMatrix, Vector>
-	oeprator*(const Vector& v) const {
-		return {*this, v};
-	}
-
-
-  private:
-  	// Empty
-  	std::size_t nrows_;
-  	std::size_t ncols_;
-
-}
 
 
 
